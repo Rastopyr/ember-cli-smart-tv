0d3ae106b094bf84771efc7c0ba7188213174474
Rastopyr <Rastopyr@gmail.com>
2016-06-24 16:32:31 +0600

    [Feature] Add horizontal scroll

--- addon/mixins/scroll/horizontal.js
+++ addon/mixins/scroll/horizontal.js
@@ -1,4 +1,54 @@
+
 import Ember from 'ember';
 
-export default Ember.Mixin.create({
+const { on, $, Mixin } = Ember;
+
+export default Mixin.create({
+  classNames: ['scroll-row'],
+
+  isPaged: true,
+
+  scrollListener: on('cellDidChange', function(options) {
+    this[
+      options.direction === 'right' ? 'triggerScrollRight' : 'triggerScrollLeft'
+    ]();
+  }),
+
+  triggerScrollRight() {
+    const isPaged = this.get('isPaged');
+    const activeElement = this.get('hoverCell.element');
+
+    const offsetLeft = $(activeElement).offset().left - $(this.element).offset().left;
+    const scrollWidth = $(activeElement)[0].scrollWidth;
+
+    const scrollLeft = $(this.element).scrollLeft();
+    const windowWidth = $(this.element).width();
+
+    if (offsetLeft + scrollWidth > windowWidth) {
+      if (isPaged) {
+        $(this.element).scrollLeft(scrollLeft + offsetLeft);
+      } else {
+        $(this.element).scrollLeft(scrollLeft + scrollWidth);
+      }
+    }
+  },
+
+  triggerScrollLeft() {
+    const isPaged = this.get('isPaged');
+    const activeElement = this.get('hoverCell.element');
+
+    const offsetLeft = $(activeElement).offset().left - $(this.element).offset().left;
+    const scrollWidth = $(activeElement)[0].scrollWidth;
+
+    const scrollLeft = $(this.element).scrollLeft();
+    const windowWidth = $(this.element).width();
+
+    if (offsetLeft <= 0) {
+      if (isPaged) {
+        $(this.element).scrollLeft(scrollLeft - windowWidth);
+      } else {
+        $(this.element).scrollLeft(scrollLeft - scrollWidth);
+      }
+    }
+  },
 });
---
+++ tests/dummy/app/components/scroll/horizontal-scroll.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+import layout from '../../templates/components/scroll/horizontal-scroll';
+
+import RowFrame from 'ember-cli-smart-tv/components/frame/row-frame';
+import ScrollMixin from 'ember-cli-smart-tv/mixins/scroll/horizontal';
+
+const { computed, on, inject } = Ember;
+
+export default RowFrame.extend(ScrollMixin, {
+  layout,
+
+  classNames: ['horizontal-scroll'],
+
+  name: 'scroll-row',
+
+  isPaged: false,
+
+  cellNames: computed(function() {
+    const rows = [];
+
+    for (let i = 1; i <= 100; i++) {
+       rows.push(`cell-${i}`);
+    }
+
+    return rows;
+  })
+});
